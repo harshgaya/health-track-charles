b0c20b3d85a2ae29c40f77649412f5df480bdcdf
Yout701 <harshkumarworld@gmail.com>
2025-03-24 14:16:11 +0530

    first commit

---
+++ android/app/src/main/java/com/fitness/fitness_health_tracker_charles/fitness_health_tracker/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fitness.fitness_health_tracker_charles.fitness_health_tracker;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
